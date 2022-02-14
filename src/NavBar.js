7fb916ebefd69cdc78aec9f44a71c14f239ac508
jcorille-21 <jcorille@uw.edu>
2022-02-13 21:20:57 -0800

    added FA icons to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,6 @@
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquarePlus, faCalendarCheck, faMessage, faLightbulb, faCircleDollarToSlot, faUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
 export default function NavBar() {
     return (
@@ -7,15 +9,46 @@ export default function NavBar() {
                 Healio
             </h1>
             <ul className="my-20">
-                <li className="py-2"><NavLink to="/">Health</NavLink></li>
-                <li className="py-2"><NavLink to="/appointments">Appointments</NavLink></li>
-                <li className="py-2"><NavLink to="/messages">Messaging</NavLink></li>
-                <li className="py-2"><NavLink to="/resources">Resources</NavLink></li>
-                <li className="py-2"><NavLink to="/billing">Billing</NavLink></li>
-                <li className="py-2"><NavLink to="/profile">Profile</NavLink></li>
+                <li className="py-2">
+                    <NavLink to="/">
+                        <FontAwesomeIcon className="mr-2" icon={faSquarePlus} size="lg" aria-label="Health" />
+                        <p className="inline ml-2">Health</p>
+                    </NavLink>
+                </li>
+                <li className="py-2">
+                    <NavLink to="/appointments">
+                        <FontAwesomeIcon className="mr-2" icon={faCalendarCheck} size="lg" aria-label="Appointments" />
+                        <p className="inline ml-2">Appointments</p>
+                    </NavLink>
+                </li>
+                <li className="py-2">
+                    <NavLink to="/messages">
+                        <FontAwesomeIcon className="mr-2" icon={faMessage} size="lg" aria-label="Messages" />
+                        <p className="inline ml-1">Messaging</p>
+                    </NavLink>
+                </li>
+                <li className="py-2">
+                    <NavLink to="/resources">
+                        <FontAwesomeIcon className="mr-2" icon={faLightbulb} size="lg" aria-label="Resources" />
+                        <p className="inline ml-2">Resources</p>
+                    </NavLink>
+                </li>
+                <li className="py-2">
+                    <NavLink to="/billing">
+                        <FontAwesomeIcon className="mr-2" icon={faCircleDollarToSlot} size="lg" aria-label="Billing" />
+                        <p className="inline ml-1">Billing</p>
+                    </NavLink>
+                </li>
+                <li className="py-2">
+                    <NavLink to="/profile">
+                        <FontAwesomeIcon className="mr-2" icon={faUser} size="lg" aria-label="Profile" />
+                        <p className="inline ml-2">Profile</p>
+                    </NavLink>
+                </li>
             </ul>
             <footer className="py-20">
-                <p>Log out</p>
+                <FontAwesomeIcon className="mr-2" icon={faRightFromBracket} size="lg" aria-label="Log out" />
+                <p className="inline ml-1">Log out</p>
             </footer>
         </nav>
     )
