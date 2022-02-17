16f0723bbac0e1a5255956c9980f37224ca31252
jcorille-21 <jcorille@uw.edu>
2022-02-16 20:10:52 -0800

    added active state to NavLinks and login state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar.js';
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Routes>
-        <Route path="/">
+  const [isSignedIn, setIsSignedIn] = useState(true); // change to false after user acc implemented
+  if (isSignedIn) {
+    return (
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/">
 
-        </Route>
-        <Route path="/appointments">
+          </Route>
+          <Route path="/appointments">
 
-        </Route>
-        <Route path="/messages">
+          </Route>
+          <Route path="/messages">
 
-        </Route>
-        <Route path="/resources">
+          </Route>
+          <Route path="/resources">
 
-        </Route>
-        <Route path="/billing">
+          </Route>
+          <Route path="/billing">
 
-        </Route>
-        <Route path="/profile">
+          </Route>
+          <Route path="/profile">
 
-        </Route>
-      </Routes>
-    </div>
-  );
+          </Route>
+        </Routes>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,52 +1,38 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquarePlus, faCalendarCheck, faMessage, faLightbulb, faCircleDollarToSlot, faUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
 export default function NavBar() {
     return (
-        <nav className="px-4 bg-[#B5FFEC] w-48 h-screen">
-            <h1 className="font-heading text-3xl font-bold py-8">
-                Healio
-            </h1>
-            <ul className="my-20">
-                <li className="py-2">
-                    <NavLink to="/">
-                        <FontAwesomeIcon className="mr-2" icon={faSquarePlus} size="lg" aria-label="Health" />
-                        <p className="inline ml-2">Health</p>
-                    </NavLink>
-                </li>
-                <li className="py-2">
-                    <NavLink to="/appointments">
-                        <FontAwesomeIcon className="mr-2" icon={faCalendarCheck} size="lg" aria-label="Appointments" />
-                        <p className="inline ml-2">Appointments</p>
-                    </NavLink>
-                </li>
-                <li className="py-2">
-                    <NavLink to="/messages">
-                        <FontAwesomeIcon className="mr-2" icon={faMessage} size="lg" aria-label="Messages" />
-                        <p className="inline ml-1">Messaging</p>
-                    </NavLink>
-                </li>
-                <li className="py-2">
-                    <NavLink to="/resources">
-                        <FontAwesomeIcon className="mr-2" icon={faLightbulb} size="lg" aria-label="Resources" />
-                        <p className="inline ml-2">Resources</p>
-                    </NavLink>
-                </li>
-                <li className="py-2">
-                    <NavLink to="/billing">
-                        <FontAwesomeIcon className="mr-2" icon={faCircleDollarToSlot} size="lg" aria-label="Billing" />
-                        <p className="inline ml-1">Billing</p>
-                    </NavLink>
-                </li>
-                <li className="py-2">
-                    <NavLink to="/profile">
-                        <FontAwesomeIcon className="mr-2" icon={faUser} size="lg" aria-label="Profile" />
-                        <p className="inline ml-2">Profile</p>
-                    </NavLink>
-                </li>
-            </ul>
-            <footer className="py-20">
+        <nav className="bg-[#B5FFEC] w-48 h-screen">
+            <h1 className="font-heading text-3xl font-bold px-3 py-8">Healio</h1>
+            <div className="flex flex-col my-20">
+                <NavLink to="/" className={({ isActive }) => isActive ? 'py-2 pl-3 pr-24 bg-[#06D6A0] font-bold' : 'py-2 px-3'}>
+                    <FontAwesomeIcon className="mr-2" icon={faSquarePlus} size="lg" aria-label="Health" />
+                    <p className="inline ml-2">Health</p>
+                </NavLink>
+                <NavLink to="/appointments" className={({ isActive }) => isActive ? 'py-2 pl-3 pr-23 bg-[#06D6A0] font-bold' : 'py-2 px-3'}>
+                    <FontAwesomeIcon className="mr-2" icon={faCalendarCheck} size="lg" aria-label="Appointments" />
+                    <p className="inline ml-2">Appointments</p>
+                </NavLink>
+                <NavLink to="/messages" className={({ isActive }) => isActive ? 'py-2 pl-3 pr-23 bg-[#06D6A0] font-bold' : 'py-2 px-3'}>
+                    <FontAwesomeIcon className="mr-2" icon={faMessage} size="lg" aria-label="Messages" />
+                    <p className="inline ml-1">Messaging</p>
+                </NavLink>
+                <NavLink to="/resources" className={({ isActive }) => isActive ? 'py-2 pl-3 pr-23 bg-[#06D6A0] font-bold' : 'py-2 px-3'}>
+                    <FontAwesomeIcon className="mr-2" icon={faLightbulb} size="lg" aria-label="Resources" />
+                    <p className="inline ml-2">Resources</p>
+                </NavLink>
+                <NavLink to="/billing" className={({ isActive }) => isActive ? 'py-2 pl-3 pr-24 bg-[#06D6A0] font-bold' : 'py-2 px-3'}>
+                    <FontAwesomeIcon className="mr-2" icon={faCircleDollarToSlot} size="lg" aria-label="Billing" />
+                    <p className="inline ml-1">Billing</p>
+                </NavLink>
+                <NavLink to="/profile" className={({ isActive }) => isActive ? 'py-2 pl-3 pr-24 bg-[#06D6A0] font-bold' : 'py-2 px-3'}>
+                    <FontAwesomeIcon className="mr-2" icon={faUser} size="lg" aria-label="Profile" />
+                    <p className="inline ml-2">Profile</p>
+                </NavLink>
+            </div>
+            <footer className="py-20 px-3">
                 <FontAwesomeIcon className="mr-2" icon={faRightFromBracket} size="lg" aria-label="Log out" />
                 <p className="inline ml-1">Log out</p>
             </footer>
