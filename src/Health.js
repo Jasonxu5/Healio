249a988dd0c66aa93157ead640d95395af89fb83
jcorille-21 <jcorille@uw.edu>
2022-02-21 15:47:51 -0800

    fixed all responsive issues for now

--- src/Health.js
+++ src/Health.js
@@ -23,9 +23,9 @@ export default function Health() {
     // "absolute left-[235px]"
     return (
         <div className="flex flex-col">
-            <header className="flex py-8 pl-[235px]">
+            <header className="relative py-8 pl-[235px]">
                 <p className="font-heading text-3xl">Overview</p>
-                <div className="absolute right-[40px]">
+                <div className="absolute bottom-[20px] right-0">
                     <img className="rounded-full inline w-10 h-10 mb-2" src={katie} alt="Katie Wang" />
                     <p className="inline text-2xl ml-2">Katie Wang</p>
                 </div>
@@ -54,7 +54,7 @@ function HealthCards(props) {
             body = <CardHasList hasDateOnRight={true} />;
         }
         return <SingleHealthCard title={cardTitle} body={body} count={index} user={user} key={cardTitle} />;
-    });
+    }); // additional rule to try: auto-rows-[minmax(min-content, max-content)]
     return (
         <div className="grid grid-cols-2 pl-[235px] gap-4">
             {healthInfoCards}
@@ -67,13 +67,13 @@ function CardHasImage(props) {
 
     if (isIntro) {
         return (
-            <div className="">
+            <div className="relative">
                 <p className="pt-2">You have new notifications! Have a nice day and remember</p>
                 <p>to take care of your health!</p>
                 <div className="mt-6 text-dark-blue">
                     <Link className="absolute p-2 font-semibold bg-dark-green rounded-lg" to='/appointments'>Schedule Appointment {'>'}</Link>
                 </div>
-                <img className="absolute top-[100px] right-[60px]" src={doctor} alt="Doctor Clip Art" />
+                <img className="absolute top-[-60px] right-0" src={doctor} alt="Doctor Clip Art" />
             </div>
         );
     } else {
@@ -95,31 +95,10 @@ function CardHasList(props) {
 function SingleHealthCard(props) {
     const { title, body, count, user } = props;
 
-    // let leftPxSize;
-    // let topPxSize;
     let introCardOnGrid;
     let stackSmallCards;
-    let widthPxSize;
     let heightPxSize;
     let headerStyle;
-
-    // // Position card based on left anchors
-    // if (count <= 1 || count === 4) {
-    //     leftPxSize = ''; // 'left-[235px] '
-    // } else {
-    //     leftPxSize = ''; // 'left-[836px] '
-    // }
-
-    // // Position card based on top anchors
-    // if (count === 0) {
-    //     topPxSize = ''; // 'top-[106px] '
-    // } else if (count < 3) {
-    //     topPxSize = ''; // 'top-[352px] '
-    // } else if (count < 4) {
-    //     topPxSize = ''; // 'top-[528px] '
-    // } else {
-    //     topPxSize = ''; // 'top-[705px] '
-    // }
 
     // Place intro card on the top
     if (count === 0) {
@@ -135,12 +114,6 @@ function SingleHealthCard(props) {
         stackSmallCards = 'row-start-2 col-start 2 col-end-[-1] self-end ';
     } else {
         stackSmallCards = '';
-    }
-    // Determine card width based on position
-    if (count === 0) {
-        widthPxSize = 'w-[1170px] ';
-    } else {
-        widthPxSize = 'w-[569px] ';
     }
 
     // Determine card height based on position
@@ -167,7 +140,7 @@ function SingleHealthCard(props) {
         headerStyle = <h2 className='font-heading text-2xl'>{title}</h2>
     }
     return (
-        <div className={"p-6 " + introCardOnGrid + stackSmallCards + widthPxSize + heightPxSize + "bg-white shadow-[2px_4px_20px_rgba(0,0,0,0.25)] rounded-[20px]"}>
+        <div className={"p-6 " + introCardOnGrid + stackSmallCards + " w-full " + heightPxSize + "bg-white shadow-[2px_4px_20px_rgba(0,0,0,0.25)] rounded-[20px]"}>
             {headerStyle}
             {body}
         </div>
