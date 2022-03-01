5d81cba7a8b6b9e9dfef6ffcddd43f91e025d465
Jason Xu <jasonxu515@gmail.com>
2022-03-01 02:23:24 -0800

    Integrate React app with Express Server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar.js';
 import Health from './Health.js';
@@ -7,6 +7,16 @@ import PlaceHolder from './Template.js';
 
 function App() {
   const [isSignedIn, setIsSignedIn] = useState(true); // change to false after user acc implemented
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    fetch('/api')
+      .then(res => res.json())
+      .then(data => setData(data.message))
+  }, [])
+
+  console.log(data)
+
   if (isSignedIn) {
     return (
       <div className="flex bg-gradient-to-br from-pale-blue to-white">
@@ -14,12 +24,14 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Health />} />
           <Route path="/appointments" element={<PlaceHolder />} />
-          <Route path="/messages" element={<Messaging />}/>
+          <Route path="/messages" element={<Messaging />} />
           <Route path="/resources" element={<PlaceHolder />} />
           <Route path="/billing" element={<PlaceHolder />} />
           <Route path="/profile" element={<PlaceHolder />} />
         </Routes>
+        <p>{!data ? "Server Error" : data}</p>
       </div>
+
     );
   } else {
     return (
