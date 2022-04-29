271edbf29a235e868def42fbe22e0c99f4d34b17
jcorille-21 <jcorille@uw.edu>
2022-04-29 15:48:20 -0700

    added filtering and responsiveness to Patient Information

--- client/src/PatientInfo.js
+++ client/src/PatientInfo.js
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function PatientInfo(props) {
     const { currUser, userHealthInfo, infoHeader } = props;
+    const [currStatus, setCurrStatus] = useState('');
+    const [currInfo, setCurrInfo] = useState('');
     const infoType = infoHeader.props.title;
+
+
+    const statusSet = new Set();
+    const statusArray = userHealthInfo.map((item, index) => {
+        if (!statusSet.has(item.status)) {
+            statusSet.add(item.status);
+            return <option value={item.status} key={index}>{item.status}</option>;
+        }
+    });
     return (
-        <div className="flex flex-col pl-[235px]">
+        <div className="md:pl-[25px] flex flex-col pl-[235px]">
             {infoHeader}
-            <FilterOptions />
-            <InfoBody currUser={currUser} userHealthInfo={userHealthInfo} infoType={infoType} />
+            <FilterOptions statuses={statusArray} currStatus={currStatus} currStatusCallback={setCurrStatus} currInfoCallback={setCurrInfo} infoType={infoType} />
+            <InfoBody currUser={currUser} currStatus={currStatus} currInfo={currInfo} userHealthInfo={userHealthInfo} infoType={infoType} />
         </div>
     )
 }
 
-function FilterOptions() {
+function FilterOptions(props) {
+    const { statuses, currStatus, currStatusCallback, currInfoCallback, infoType } = props;
+    const handleOptionChange = (event) => {
+        currStatusCallback(event.target.value);
+    };
+
+    const handleTextChange = (event) => {
+        currInfoCallback(event.target.value.toLowerCase());
+    };
     return (
         <div>
             <form className="flex gap-3 mt-5 mb-10">
                 <label className="absolute left-[-100vw]">Type something here...</label>
                 <input className="p-[12px] w-[250px] rounded-[15px] bg-grey placeholder:text-black"
-                    placeholder="Search here..." aria-label="Filter your health information" autoComplete="off" />
-                <select className="p-[12px] w-[200px] rounded-[15px] bg-grey">
-                    <option value="volvo">Filter by status...</option>
-                    <option value="saab">Saab</option>
-                    <option value="mercedes">Mercedes</option>
-                    <option value="audi">Audi</option>
+                    onChange={handleTextChange} placeholder={'Search for ' + infoType + '...'} aria-label="Filter your health information" autoComplete="off" />
+                <select className="p-[12px] w-[200px] rounded-[15px] bg-grey" onChange={handleOptionChange} value={currStatus}>
+                    <option value="">Filter by status...</option>
+                    {statuses}
                 </select>
             </form>
         </div>
@@ -31,27 +48,29 @@ function FilterOptions() {
 }
 
 function InfoBody(props) {
-    const { currUser, userHealthInfo, infoType } = props;
-
-
-    if (userHealthInfo.length === 0) {
-        return <p>No {infoType.toLowerCase()} are available for {currUser.firstName}.</p>
-    } else {
-        const healthInfoArray = userHealthInfo.map((item, index) => {
+    const { currUser, currStatus, currInfo, userHealthInfo, infoType } = props;
+    
+    const healthInfoArray = userHealthInfo.map((item, index) => {
+        const itemLowercase = item.name.toLowerCase();
+        if ((currStatus === '' || currStatus === item.status) && itemLowercase.includes(currInfo)) {
             return <SingleHealthInfoItem
                 dateObject={item.date}
                 name={item.name}
                 doctor={item.doctor}
                 status={item.status}
                 key={index} />
-        });
+        }
+    })
+    if (healthInfoArray.length === 0) {
+        return <p>No {infoType.toLowerCase()} are available for {currUser.firstName}.</p>
+    } else {
         return (
             <div>
-                <div className="flex relative mb-4 ml-5 justify-between font-heading text-2xl text-dark-blue">
+                <div className="sm:grid-cols-[200px,250px] md:grid-cols-[150px,300px,250px] grid grid-cols-[220px,350px,225px,250px] mb-4 ml-5 font-heading text-2xl text-dark-blue">
                     <p className="">Date</p>
-                    <p className="absolute right-[660px]">{infoType.substring(0, infoType.length - 1)}</p>
-                    <p className="absolute right-[360px]">Doctor</p>
-                    <p className="absolute right-[110px]">Status</p>
+                    <p className="">{infoType.substring(0, infoType.length - 1)}</p>
+                    <p className="sm:hidden">Doctor</p>
+                    <p className="md:hidden">Status</p>
                 </div>
                 <div className="grid gap-8">
                     {healthInfoArray}
@@ -76,11 +95,11 @@ function SingleHealthInfoItem(props) {
     const date = month + '/' + day + '/' + year;
 
     return (
-        <div className="flex relative justify-between font-heading text-2xl w-[95%] p-[20px] bg-white shadow-[2px_4px_20px_rgba(0,0,0,0.25)] rounded-[20px]">
+        <div className="sm:grid-cols-[200px,250px] md:grid-cols-[150px,300px,250px] grid grid-cols-[220px,350px,225px,250px] relative font-heading text-2xl w-[95%] p-[20px] bg-white shadow-[2px_4px_20px_rgba(0,0,0,0.25)] rounded-[20px]">
             <p className="">{date}</p>
-            <p className="absolute right-[600px]">{name}</p>
-            <p className="absolute right-[300px]">{doctor}</p>
-            <p className="absolute right-[50px]">{status}</p>
+            <p className="">{name}</p>
+            <p className="sm:hidden">{doctor}</p>
+            <p className="md:hidden">{status}</p>
         </div>
     );
 }
