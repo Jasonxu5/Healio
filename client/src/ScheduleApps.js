e841f8fdb5dd82e7b19b0005ba4420207827b8b9
jcorille-21 <jcorille@uw.edu>
2022-05-08 00:09:57 -0700

    added calendar and style ALMOST DONE

--- client/src/ScheduleApps.js
+++ client/src/ScheduleApps.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Calendar } from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './calendar.css';
+
+// Because Calendar is hard to use with Tailwind...
+// ...I have to painfully relearn normal CSS
 
 export default function ScheduleApps(props) {
     const { currUser, scheduleAppsHeader } = props;
     return (
         <div className="md:pl-[25px] flex flex-col pl-[235px]">
             {scheduleAppsHeader}
+            <AppCalendar currUser={currUser} />
         </div>
     );
+}
+
+function AppCalendar(props) {
+    const { currUser } = props;
+    return <Calendar
+        className="animate-popup h-screen"
+        prev2Label={null}
+        next2Label={null}
+        view='month'
+        tileClassName="transition h-[14.2vh]"
+    />;
 }
