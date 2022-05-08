5d45752ccd8a6f0103ba7e8c76aa3a0175054fb5
jcorille-21 <jcorille@uw.edu>
2022-05-08 02:50:36 -0700

    some minor modal changes

--- client/src/AddUserModal.js
+++ client/src/AddUserModal.js
@@ -40,7 +40,7 @@ export default function AddUserModal(props) {
     }
     
     return (
-        <Modal className="sm:w-[250px] md:w-[500px] animate-popup absolute left-[25%] top-[10%] w-[750px] h-[500px] p-5 bg-white border-2 border-black rounded-[15px] shadow-[4px_4px_4px_rgba(0,0,0,0.25)]" show={isAddUserClicked} ref={addUserRef}>
+        <Modal className="sm:w-[250px] md:w-[500px] animate-popup absolute left-[25%] top-[10%] w-[750px] p-5 bg-white border-2 border-black rounded-[15px] shadow-[4px_4px_4px_rgba(0,0,0,0.25)]" show={isAddUserClicked} ref={addUserRef}>
             <FontAwesomeIcon className="text-2xl mb-2 hover:text-[#FF0000] hover:cursor-pointer" onClick={() => addUserCallback(false)} icon={faX} size="lg" aria-label="Close icon" />
             <Modal.Header className="text-center" closeButton>
                 <Modal.Title className="font-heading text-3xl">Add a family member</Modal.Title>
