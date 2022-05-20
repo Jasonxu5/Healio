c3e5e2aa3eec58609d81c9c53bc16121ffd18e9f
Jason Xu <jasonxu515@gmail.com>
2022-05-19 23:57:14 -0700

    Update server endpoint

--- client/src/serverEndpoint.js
+++ client/src/serverEndpoint.js
@@ -1,2 +1,2 @@
-// export const serverEndpoint = "https://infinite-temple-26751.herokuapp.com/api/v1/"
-export const serverEndpoint = "http://localhost:5000/api/v1/"
+export const serverEndpoint = "https://infinite-temple-26751.herokuapp.com/api/v1/"
+// export const serverEndpoint = "http://localhost:5000/api/v1/"
